fc6952c827d30382bfcc723175a09d15f40536ab
Marina Karmanova <marina.karmanova@acumatica.com>
2026-04-11 17:28:53 +0300

    task4

--- tasks/task4/check-dns.sh
+++ tasks/task4/check-dns.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-set -e
 
 echo "▶️ Running in-cluster DNS test..."
 
-kubectl run dns-test --rm -it \
-  --image=busybox \
-  --restart=Never \
-  -- wget -qO- http://booking-service/ping && echo "✅ Success" || echo "❌ Failed"
+kubectl run dns-test --rm -it --image=busybox --restart=Never -- wget -qO- http://booking-service/ping && echo "✅ Success" || echo "❌ Failed"
